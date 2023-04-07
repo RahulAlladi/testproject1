4faf2fa344cfbd214fb808cc36575ae16f807014
RahulAlladi <83696601+RahulAlladi@users.noreply.github.com>
2023-04-07 15:29:26 +0530

    commit 5

--- testproject1/settings.py
+++ testproject1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h3zyixfpilrlie(-&6rn&0yjz8bzip^n0z#=u=u$^p4t3cnj98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
